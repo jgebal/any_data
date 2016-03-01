421f4160eb81b47cca43231a3a0d38378b9557e4
Jacek Gębal <jgebal@gmail.com>
2016-03-01 01:07:57 +0000

    Fixed reinstall.rb after moving to different folder

--- support/reinstall.rb
+++ support/reinstall.rb
@@ -4,43 +4,43 @@
 require_relative 'execute_sqlplus_file'
 
 [
-  '../../../sql/uninstall.sql',
-  '../../../sql/helpers/string_array.sql',
-  '../../../sql/helpers/any_data_formatter.pks',
-  '../../../sql/helpers/any_data_formatter.pkb',
+  '../../sql/uninstall.sql',
+  '../../sql/helpers/string_array.sql',
+  '../../sql/helpers/any_data_formatter.pks',
+  '../../sql/helpers/any_data_formatter.pkb',
 
-  '../../../sql/core/any_data.sql',
-  '../../../sql/core/any_data_attribute.sql',
-  '../../../sql/core/any_data_tab.sql',
+  '../../sql/core/any_data.sql',
+  '../../sql/core/any_data_attribute.sql',
+  '../../sql/core/any_data_tab.sql',
 
-  '../../../sql/core/any_data_family_compound.sql',
-  '../../../sql/core/any_data_object.sql',
-  '../../../sql/core/any_data_collection.sql',
+  '../../sql/core/any_data_family_compound.sql',
+  '../../sql/core/any_data_object.sql',
+  '../../sql/core/any_data_collection.sql',
 
-  '../../../sql/core/any_data_family_numeric.sql',
-  '../../../sql/core/any_data_number.sql',
-  '../../../sql/core/any_data_bdouble.sql',
-  '../../../sql/core/any_data_bfloat.sql',
+  '../../sql/core/any_data_family_numeric.sql',
+  '../../sql/core/any_data_number.sql',
+  '../../sql/core/any_data_bdouble.sql',
+  '../../sql/core/any_data_bfloat.sql',
 
-  '../../../sql/core/any_data_family_string.sql',
-  '../../../sql/core/any_data_varchar2.sql',
-  '../../../sql/core/any_data_varchar.sql',
-  '../../../sql/core/any_data_char.sql',
-  '../../../sql/core/any_data_clob.sql',
+  '../../sql/core/any_data_family_string.sql',
+  '../../sql/core/any_data_varchar2.sql',
+  '../../sql/core/any_data_varchar.sql',
+  '../../sql/core/any_data_char.sql',
+  '../../sql/core/any_data_clob.sql',
 
-  '../../../sql/core/any_data_family_date.sql',
-  '../../../sql/core/any_data_date.sql',
+  '../../sql/core/any_data_family_date.sql',
+  '../../sql/core/any_data_date.sql',
 
-  '../../../sql/core/any_data_family_raw.sql',
-  '../../../sql/core/any_data_raw.sql',
-  '../../../sql/core/any_data_blob.sql',
+  '../../sql/core/any_data_family_raw.sql',
+  '../../sql/core/any_data_raw.sql',
+  '../../sql/core/any_data_blob.sql',
 
-  '../../../sql/core/any_data_intervalds.sql',
-  '../../../sql/core/any_data_intervalym.sql',
+  '../../sql/core/any_data_intervalds.sql',
+  '../../sql/core/any_data_intervalym.sql',
 
-  '../../../sql/converter/any_type_mapper.sql',
-  '../../../sql/converter/any_data_builder.pks',
-  '../../../sql/converter/any_data_builder.pkb',
+  '../../sql/converter/any_type_mapper.sql',
+  '../../sql/converter/any_data_builder.pks',
+  '../../sql/converter/any_data_builder.pkb',
 ].each { |file| execute_sqlplus_file(file) }
 
 
