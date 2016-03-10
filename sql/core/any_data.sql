fb52c772ebdd00daa6a72e45c5ba0de3149a591d
Jacek Gębal <jgebal@gmail.com>
2016-03-10 23:29:18 +0000

    Updated test results. Added type name to not supporter add_element method

--- sql/core/any_data.sql
+++ sql/core/any_data.sql
@@ -1,38 +1,3 @@
-create or replace type any_data authid current_user as object(
-   type_code      number(38, 0),
-   type_name      varchar2(100),
-   self_type_name varchar2(100),
-   final member function to_string return varchar2,
-   not instantiable member function to_string_array( p_separator varchar2 := null ) return string_array,
-   not instantiable member function get_self_family_name return varchar2,
-   member function get_self_type_name return varchar2,
-   member procedure add_element( self in out nocopy any_data, p_attribute any_data ),
-   member function get_element( p_position integer ) return any_data,
-   member function get_elements_count return integer,
-   member function compare_internal( p_other any_data ) return integer,
-   order member function compare( p_other any_data ) return integer,
-   member function equals( p_other any_data ) return boolean,
-   final member function not_equals( p_other any_data ) return boolean,
-   member function greater_than( p_other any_data ) return boolean,
-   member function less_than( p_other any_data ) return boolean,
-   member function greater_equal_to( p_other any_data ) return boolean,
-   member function less_equal_to( p_other any_data ) return boolean,
-   final member function eq( p_other any_data ) return boolean,
-   final member function "= "( p_other any_data ) return boolean,
-   final member function neq( p_other any_data ) return boolean,
-   final member function "!="( p_other any_data ) return boolean,
-   final member function "<>"( p_other any_data ) return boolean,
-   final member function gt( p_other any_data ) return boolean,
-   final member function "> "( p_other any_data ) return boolean,
-   final member function lt( p_other any_data ) return boolean,
-   final member function "< "( p_other any_data ) return boolean,
-   final member function ge( p_other any_data ) return boolean,
-   final member function ">="( p_other any_data ) return boolean,
-   final member function le( p_other any_data ) return boolean,
-   final member function "<="( p_other any_data ) return boolean
-) not final not instantiable;
-/
-
 create or replace type body any_data as
 
    final member function to_string return varchar2 is
@@ -55,7 +20,7 @@ create or replace type body any_data as
 
    member procedure add_element( self in out nocopy any_data, p_attribute any_data ) is
       begin
-         raise_application_error(-2000, 'Feature not available for this type');
+         raise_application_error( -20000, 'Feature not available for ' || get_self_type_name( ) || ' type' );
       end;
 
    member function get_element( p_position integer ) return any_data is
