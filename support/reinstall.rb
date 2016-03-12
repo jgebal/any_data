962eeab2b9ff8e5e6178032959317360f75c8813
Jacek Gębal <jgebal@gmail.com>
2016-03-12 01:25:08 +0000

    Change reinstall.rb to have full log of sqlplus command outputs

--- support/reinstall.rb
+++ support/reinstall.rb
@@ -1,2 +1,4 @@
-`sqlplus generic_util/generic_util@xe @sql/uninstall.sql`
-`sqlplus generic_util/generic_util@xe @sql/install.sql`
+# `sqlplus generic_util/generic_util@xe @sql/uninstall.sql`
+# `sqlplus generic_util/generic_util@xe @sql/install.sql`
+system('sqlplus generic_util/generic_util@xe @sql/uninstall.sql')
+system('sqlplus generic_util/generic_util@xe @sql/install.sql')
