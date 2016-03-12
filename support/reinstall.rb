1c16ff6480093a7f1f2692fc6628d81df04861af
Jacek Gębal <jgebal@gmail.com>
2016-03-12 10:41:27 +0000

    Suppressing output to screen for reinstall initiated by unit tests

--- support/reinstall.rb
+++ support/reinstall.rb
@@ -1,4 +1,4 @@
-# `sqlplus generic_util/generic_util@xe @sql/uninstall.sql`
-# `sqlplus generic_util/generic_util@xe @sql/install.sql`
-system('sqlplus generic_util/generic_util@xe @sql/uninstall.sql')
-system('sqlplus generic_util/generic_util@xe @sql/install.sql')
+`sqlplus generic_util/generic_util@xe @sql/uninstall.sql` #no output to screen
+`sqlplus generic_util/generic_util@xe @sql/install.sql`   #no output to screen
+# system('sqlplus generic_util/generic_util@xe @sql/uninstall.sql') #with output to screen
+# system('sqlplus generic_util/generic_util@xe @sql/install.sql')   #with output to screen
