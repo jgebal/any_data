ab1c8ce759b047468ccb34631e34a24be7a73e47
Jacek Gębal <jgebal@gmail.com>
2016-03-18 23:33:27 +0000

    Fixed datatype issue for timestamp field (need to use timestamp unconstrained PLSQL parameter in constructor but the field cant use that subtype.

--- sql/core/any_data_timestamp.sql
+++ sql/core/any_data_timestamp.sql
@@ -1,5 +1,4 @@
 create or replace type any_data_timestamp authid current_user under any_data_family_date(
---   data_value timestamp_unconstrained, -- this field doesn't work like timestamp(9), it's actually a date field (BUG)?
    data_value timestamp(9),
    overriding member function to_string_array( p_separator varchar2 := null ) return string_array,
    constructor function any_data_timestamp( self in out nocopy any_data_timestamp, p_data timestamp_unconstrained ) return self as result
