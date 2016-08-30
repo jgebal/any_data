2512dbdbb57558d3f2127e4e6d3132e197eb61e5
Jacek Gębal <jgebal@gmail.com>
2016-08-30 23:23:16 +0100

    Updated test script to include profiling of comparators. Added test blocks for dbms_crypto Added grant on dbms_Crupt to deployment user (Travis)

--- sources/test.sql
+++ sources/test.sql
@@ -45,8 +45,9 @@ declare
    a anydata;
    d any_data;
    v_loops integer := 1000;
+   v_string varchar2(32767);
 begin
---   dbms_profiler.START_PROFILER('any_data '||v_loops||' times');
+   dbms_profiler.START_PROFILER('any_data_bulder.build(nested_object_with_two_number_fields)'||v_loops||' times');
    x := test_number_object(1,2);
    y := test_object(x);
    z := test_object_2(y);
@@ -54,27 +55,87 @@ begin
    for i in 1 .. v_loops loop
       d := any_data_builder.build(a);
    end loop;
-   dbms_output.put_line( d.to_string() );
---   dbms_profiler.stop_PROFILER();
+   dbms_profiler.stop_PROFILER();
+   dbms_profiler.START_PROFILER('any_data.to_string(nested_object_with_two_number_fields)'||v_loops||' times');
+   v_string := d.to_string();
+   dbms_profiler.stop_PROFILER();
+   dbms_output.put_line( v_string );
 end;
 /
 
+declare
+   x sys_refcursor;
+   y sys_refcursor;
+   a any_data;
+   b any_data;
+   v_result boolean;
+   v_string varchar2(32767);
+begin
+   open x for select * from user_procedures;
+   open y for select * from user_procedures;
+   dbms_profiler.START_PROFILER('any_data_bulder.build( select 192 rows / 14 cols = 2688 fields )');
+   a := any_data_builder.build(x);
+   dbms_profiler.stop_PROFILER();
+
+   dbms_profiler.START_PROFILER('any_data_bulder.build( select 192 rows / 14 cols = 2688 fields )');
+   b := any_data_builder.build(y);
+   dbms_profiler.stop_PROFILER();
+
+   dbms_profiler.START_PROFILER('any_data.eq( select 192 rows / 14 cols = 2688 fields )');
+   v_result := b.eq(a);
+   dbms_profiler.stop_PROFILER();
+
+   dbms_profiler.START_PROFILER('any_data.to_string(select 192 rows / 14 cols = 2688 fields)'||' times');
+   v_string := b.to_string();
+   dbms_profiler.stop_PROFILER();
+   dbms_output.put_line( v_string );
+end;
+/
 
 declare
-   x test_number_object;
-   y test_object;
-   z test_object_2;
-   a anydata;
-   d any_data;
+   x sys_refcursor;
+   y sys_refcursor;
+   a any_data;
+   b any_data;
+   v_result boolean;
+   v_string varchar2(32767);
 begin
-   x := test_number_object(1,2);
-   y := test_object(x);
-   z := test_object_2(y);
-   a := anydata.convertObject(z);
-   for i in 1 .. 10 loop
-      d := any_data_builder.build(a);
-   end loop;
-   dbms_output.put_line( d.to_string() );
+   open x for select * from user_objects;
+   open y for select * from user_objects;
+   dbms_profiler.START_PROFILER('any_data_bulder.build( select 98 rows / 14 cols = 1372 fields )');
+   a := any_data_builder.build(x);
+   dbms_profiler.stop_PROFILER();
+
+   dbms_profiler.START_PROFILER('any_data_bulder.build( select 98 rows / 14 cols = 1372 fields )');
+   b := any_data_builder.build(y);
+   dbms_profiler.stop_PROFILER();
+
+   dbms_profiler.START_PROFILER('any_data.eq( select 98 rows / 14 cols = 1372 fields )');
+   v_result := b.eq(a);
+   dbms_profiler.stop_PROFILER();
+
+   dbms_profiler.START_PROFILER('any_data.to_string(select 98 rows / 14 cols = 1372 fields)'||' times');
+   v_string := b.to_string();
+   dbms_profiler.stop_PROFILER();
+   dbms_output.put_line( v_string );
+end;
+/
+
+declare
+   x sys_refcursor;
+   y sys_refcursor;
+   a any_data;
+   b any_data;
+   v_result boolean;
+   v_string varchar2(32767);
+begin
+   open x for select * from user_objects;
+   open y for select * from user_objects;
+   a := any_data_builder.build(x);
+   b := any_data_builder.build(y);
+   v_result := b.eq(a);
+   v_string := b.to_string();
+   dbms_output.put_line( v_string );
 end;
 /
 
@@ -106,3 +167,23 @@ order by runid desc, plsql_profiler_data.total_time desc
 ;
 
 
+declare
+  x varchar2(20);
+begin
+  for i in 1 .. 5000 loop
+     x := dbms_obfuscation_toolkit.md5( input_string => i);
+  end loop;
+end;
+
+declare
+  x raw(16);
+begin
+  for i in 1 .. 5000 loop
+   x := sys.dbms_crypto.hash(to_char(i), dbms_crypto.HASH_MD5);
+  end loop;
+end;
+
+
+select * From dba_tab_privs where table_name = 'DBMS_CRYPTO';
+
+--grant execute on dbms_crypto to dba;
